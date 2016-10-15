c2b9a21d51e0cd26f84b331d0fc7a6a060454d69
Joseph Howard <joesf4@gmail.com>
2016-10-15 15:08:20 -0700

    Audio conversion mostly finished.

--- routes/getSpeech.js
+++ routes/getSpeech.js
@@ -3,11 +3,12 @@ var router = express.Router();
 var https = require('https');
 var fs = require('fs');
 var ytdl = require('ytdl-core');
+var ffmpeg = require('fluent-ffmpeg');
 
 /* GET home page. */
 router.post('/', function(req, res) {
 
-  	var params = '&part=snippet&q=trump';
+  	/*var params = '&part=snippet&q=trump';
 	
 	var apikey = 'AIzaSyCR_YpfrlnM5HimvJk0iBfcmY0gv2rx764'
 	var youtubeUrl = 'https://www.googleapis.com/youtube/v3/search?id=7lCDEYXw3mM&key=' + apikey + params;
@@ -27,16 +28,32 @@ router.post('/', function(req, res) {
             
         });
         
-    });
+    });*/
 	
+	
+	console.log(req.body.suggest);
 	
 	var url = 'https://www.youtube.com/watch?v=E1ysFmRPaTg';
 	
 	//ytdl.getInfo(url, null, function (err, info) {console.log(info)});
 	
-	ytdl(url, { filter: function(format) { 		  //console.log("\n\nFORMAT------>: " + JSON.stringify(format, null, 4) + "\n\n");
-												  return format.resolution == undefined; } })
-  	.pipe(fs.createWriteStream('sound.ogg'));
+	var videopath = 'sound.mp4';
+	var soundpath = 'sound.ogg';
+	
+	var stream = ytdl(url, { filter: function(format) { 		  //console.log("\n\nFORMAT------>: " + JSON.stringify(format, null, 4) + "\n\n");
+												  return format.container == 'mp4'; } });
+  	//.pipe(fs.createWriteStream(videopath));
+  	
+  	var proc = new ffmpeg({source: stream});
+  	
+  	proc.withAudioCodec('libvorbis').noVideo().toFormat('ogg').saveToFile(soundpath, function(stdout, stderr) {
+                				console.log('file has been converted succesfully');
+                				//res.send({status: soundpath});
+            			});
+            			
+    res.send({status: soundpath});
+    console.log('Finished!');
+            
 });
 
 module.exports = router;
